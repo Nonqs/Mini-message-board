e8553de49332e562b2ffa7a1b49fb2bf3b17e2b1
Tomas Trujillo <tomastru114@gmail.com>
2024-01-22 17:20:02 +0100

    -create server

---
+++ index.js
@@ -0,0 +1,20 @@
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+const express = require("express");
+
+const server = express();
+
+server.listen(8080, () => {
+
+})
